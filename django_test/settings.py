acefd75aafe1870f7a415ead50e532bd099d9a4e
s10196 <s10196@ubuntu.com>
2015-10-05 10:26:58 +0800

    update database path to relative path,

--- django_test/settings.py
+++ django_test/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/s10196/proj/django-rb/db/rb.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'db/rb.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
